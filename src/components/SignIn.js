b616956286dd76faf2e0568498a5246d03e051b3
samsundar <samradnus2001@gmail.com>
2022-09-04 21:22:14 +0530

    Updated CSS

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -5,8 +5,34 @@ import google from './../Images/google.png'
 import group from './../Images/modal2.png'
 import balance1 from './../Images/balance1.png'
 import balance2 from './../Images/balance2.png'
+import vector1 from './../Images/Vector(1).png'
+import vector2 from './../Images/Vector(2).png'
+import vector3 from './../Images/Vector(3).png'
+import vector4 from './../Images/Vector(4).png'
+import vector5 from './../Images/Vector(5).png'
+import vector6 from './../Images/Vector(6).png'
 import { Link } from 'react-router-dom'
+import { useState,useEffect } from 'react'
+import title from './../Images/title.png'
+import body from './../Images/body.png'
 const SignIn = () => {
+    const [mobile,setMobile]=useState(window.innerWidth<851);
+    const [dimensions,setDimensions]=useState({
+      width:window.innerWidth,
+      height:window.innerHeight
+    })
+    const handleResize=()=>{
+         setDimensions({
+          width:window.innerWidth,
+          height:window.innerHeight
+         }) ;
+    }
+    useEffect(()=>{
+      window.addEventListener("resize",handleResize,false);
+      dimensions.width>851?setMobile(true):setMobile(false);
+    },[dimensions.width]);
+
+    
     return (
         <div>
             <div id="form">
@@ -19,11 +45,11 @@ const SignIn = () => {
                    </div>
                     <button  id="social">
                         <img alt="logo" src={google} height='20' width='20'></img>
-                        <p>Sign In with Google</p>   
+                        {mobile&&<p>Sign In with Google</p>}   
                     </button>
                     
                     
-                    <div ><p id="strike"><span>Or with email</span></p></div>
+                    <div ><p id="strike">{mobile?<span>Or with email</span>:<span>or</span>}</p></div>
 
                     <div style={{marginTop:'50px'}} className="form-floating mb-3">
                         <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
@@ -39,7 +65,7 @@ const SignIn = () => {
                         <label className="form-check-label" for="exampleCheck1">Remember Me</label>
                     </div>
                     
-                        <Link to="/">Forgot Password</Link>
+                        <Link id="forgot" to="/">Forgot Password</Link>
                     </div>
                     <div id="sign">
                         <button type="button" id="wideBtn" className="btn btn-primary">Sign-In</button>
@@ -55,7 +81,11 @@ const SignIn = () => {
                     <p id="second">Copyright 2022</p>
                 </div>
             </div>
-            <div id="send" style={{ backgroundImage: `url(${Form})` }}>
+
+           { mobile&&<div id="send" style={{ backgroundImage: `url(${Form})` }}>
+                <div id="vector1" >
+                     <img src={vector1} height='100%' width='100%'/>
+                </div>
                 <div id="group" >
                     <img src={group} height='100%' width='100%'/>
                 </div>
@@ -65,7 +95,27 @@ const SignIn = () => {
                 <div id="balance2" >
                     <img src={balance2} height='100%' width='100%'/>
                 </div>
-            </div>
+                <div id="vector5" >
+                     <img src={vector5} height='100%' width='100%'/>
+                </div>
+                <div id="vector6" >
+                     <img src={vector6} height='100%' width='100%'/>
+                </div>
+                <div id="balance3" >
+                    <h1>Get better with Money</h1>
+                </div>
+                <div id="balance4" >
+                    <p>
+                        Overplay helps you reach your saving goals,earn cash back offers,Go to disclaimer
+                        for more details and get paychecks up to two days early.Get a <small>$20 </small>
+                        bonus when you receive qaulifying direct deposits
+                    </p>
+                </div>
+                <div id="vector4" >
+                     <img src={vector4} height='100%' width='100%'/>
+                </div>
+            </div>}
+            
         </div>
     )
 }
